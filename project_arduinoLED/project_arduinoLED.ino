dab764fad789acbc898f7bf05d9c86ac326cb1e9
Linda Devita Anggraini <lindadevitaanggraini@gmail.com>
2026-06-06 13:10:40 +0700

    linda sudah menambahkan device led merah

--- project_arduinoLED/project_arduinoLED.ino
+++ project_arduinoLED/project_arduinoLED.ino
@@ -25,30 +25,22 @@ void loop() {
     //2= kuning menyala
     else if (perintah == '2'){
        digitalWrite(led_kuning, HIGH);
-}
-
-
+      }
     //3= hijau menyala
     else if (perintah == '3'){
         digitalWrite(led_hijau, HIGH);
-    }
-
-
-
-
-
+      }
    //4= semua mati
-
-
+   else if (perintah == '4'){
+   digitalWrite(led_merah, LOW);
    digitalWrite(led_kuning, LOW);
    digitalWrite(led_hijau, LOW);
-
-
-
-
+   }
    //5= semua nyala
+   else if (perintah == '5'){
+   digitalWrite(led_merah, HIGH);
+   digitalWrite(led_kuning, HIGH);
+   digitalWrite(led_hijau, HIGH);
+   }
   }
-  
-  digitalWrite(led_kuning, HIGH);
-  digitalWrite(led_hijau, HIGH);
 }
